84513e7ad2d853f86ac440b568efd47e5639e96f
Sameer Singh <ssingh@markettime.com>
2023-09-29 16:36:14 +0530

    Till Props

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import {a, b, c} from './module2.mjs';
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ module2.mjs
@@ -0,0 +1,7 @@
+const a = "Sameer";
+const b = "Navneet";
+const c = "aayushi"
+
+export {a};
+export {b};
+export {c};
